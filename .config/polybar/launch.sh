8923f39b936cc7ac13ce9187f83b975a909333b1
Erik <erik.lillehagen@purplescout.se>
2021-04-08 15:44:45 +0200

    add launch script for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
@@ -7,5 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
-# Launch bar1
-polybar -c ~/.config/polybar/config main &
+# Launch 
+MONITOR=$(polybar -m | awk -F'[:+]' '/primary/{print $1}');
+polybar -c  ~/.config/polybar/config --reload main &
+echo launching main on $MONITOR
+
+for m in $(polybar -m | awk -F'[:+]' !'/primary/{print $1}'); do
+	MONITOR=$m polybar -c  ~/.config/polybar/config --reload secondary &
+	echo launching secondary on $MONITOR
+done
+
